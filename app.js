4fd8c65d2a7590f189e3c9cafbc3ac7c4b4f6110
kattah <kattahpro@gmail.com>
2022-06-22 05:55:09 +0900

    yo

---
+++ app.js
@@ -0,0 +1,33 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 3001
+
+
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+// Set Views
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+
+app.get('', (req, res) => {
+    res.render('index', { text: 'This is EJS' })
+})
+
+app.get('/commands', (req, res) => {
+    res.render('commands', { text: 'Commands' })
+})
+
+
+
+
+
+// Listen on port 3001
+app.listen(port, () => console.info(`Listening on port ${port}`))
