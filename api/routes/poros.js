6ed451feab85cbda58b2151adf53866610ebfd12
kattah <kattahpro@gmail.com>
2022-06-24 01:58:31 +0900

    yo

--- api/routes/poros.js
+++ api/routes/poros.js
@@ -8,15 +8,29 @@ router.get("/lookup/:user", async (req, res) => {
     }
 
     const lastUsage = await bot.Redis.get(`poro:${req.params.user}`);
+    const channelData = await bot.DB.poroCount.findOne({ username: req.params.user }).exec();
+
+    if (!channelData) {
+        return res.status(404).send("User not found");
+    }
 
     if (!lastUsage) {
         res.json({
             cooldown: false,
+            lastUsage: lastUsage,
+            poroCount: channelData.poroCount,
+            joinedAt: channelData.joinedAt,
+            poroPrestige: channelData.poroPrestige,
         });
     } else {
         res.json({
             cooldown: true,
             lastUsage: lastUsage,
+            poro: { 
+            poroCount: channelData.poroCount,
+            joinedAt: channelData.joinedAt,
+            poroPrestige: channelData.poroPrestige,
+            }
         });
     }
 });
