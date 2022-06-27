24d6aec3cd3c777534cacbd07cdae9d90d9a56c8
kattah <kattahpro@gmail.com>
2022-06-28 05:25:12 +0900

    yo

--- app.js
+++ app.js
@@ -52,16 +52,16 @@ app.get('/code', (req, res) => {
 app.get('/leaderboard', (req, res) => {
     bot.DB.poroCount.find({}).exec(function(err, kekw) {
         if (err) throw err;
-        res.render('leaderboard', { porosLB1: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(0, 1)).map((user) => `${user.username} - ${user.poroCount} `) 
-        , porosLB2: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(1, 2)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB3: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(2, 3)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB4: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(3, 4)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB5: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(4, 5)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB6: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(5, 6)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB7: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(6, 7)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB8: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(7, 8)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB9: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(8, 9)).map((user) => `${user.username} - ${user.poroCount} `),
-        porosLB10: ((kekw.sort((a, b) => b.poroCount - a.poroCount)).slice(9, 10)).map((user) => `${user.username} - ${user.poroCount} `),
+        res.render('leaderboard', { porosLB1: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(0, 1)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `) 
+        , porosLB2: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(1, 2)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB3: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(2, 3)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB4: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(3, 4)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB5: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(4, 5)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB6: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(5, 6)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB7: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(6, 7)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB8: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(7, 8)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB9: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(8, 9)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
+        porosLB10: ((kekw.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(9, 10)).map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount} `),
 
     })
     })
@@ -83,7 +83,7 @@ app.get('/rank', (req, res) => {
     bot.DB.poroCount.find({}).exec(function(err, kekw3) {
         if (err) throw err;
         res.render('rank', {
-            xd2: (((kekw3.sort((a, b) => b.poroCount - a.poroCount)).slice(0, 5000000)).findIndex((user) => user.username == req.query.user) + 1),
+            xd2: (((kekw3.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount)).slice(0, 5000000)).findIndex((user) => user.username == req.query.user) + 1),
             xd3: (kekw3.length.toLocaleString())
         })
     })
