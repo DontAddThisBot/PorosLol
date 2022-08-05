1512c70c3f775a7e84ed75938557b8a7bf4cdf1a
Kattah <kattahpro@gmail.com>
2022-08-05 22:01:06 +0900

    code review

--- api/server.js
+++ api/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const poros = require("./routes/poros");
+const join = require("./routes/join");
+const part = require("./routes/part");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(poros);
+app.use(join)
+app.use(part)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
