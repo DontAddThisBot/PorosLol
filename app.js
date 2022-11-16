8882ee642175ff63d131ca28efb8176f22ca7838
Kattah <kattahpro@gmail.com>
2022-11-17 07:32:54 +0900

    test

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const redis = require("./util/redis.js");
 const humanizeDuration = require("./humanizeDuration");
 const axios = require("axios");
 const nodeFetch = require("node-fetch");
+("test");
 
 const join = require("./routes/join");
 const part = require("./routes/part");
